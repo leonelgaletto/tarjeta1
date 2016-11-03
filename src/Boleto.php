6e5c6703bd86a05afc7c5921b53f417b0b995358
leonelgaletto <leonel.galetto@gmail.com>
2016-11-03 13:02:23 -0300

    Update and rename el bondi.php to Boleto.php

---
+++ src/Boleto.php
@@ -0,0 +1,33 @@
+<?php
+	
+namespace Poli\Tarjeta;
+class Boleto {
+	protected $fecha,$saldo,$tipo,$tipos = array(1 => "Normal" , 2 => "Plus" , 3 => "Medio"),$linea,$costo,$idTarjeta;
+	public function __construct($fecha,$tipo,$costo,$saldo,$linea,$id){
+		$this->fecha=$fecha;
+		$this->tipo=$tipo;
+		$this->costo=$costo;
+		$this->saldo=$saldo;
+		$this->linea=$linea;
+		$this->idTarjeta=$id;
+	}
+	public function getCosto(){
+		return $this->costo;
+	}
+	public function getFecha(){
+		return $this->fecha;
+	}
+	public function getLinea(){
+		return $this->linea;
+	}
+	public function getId(){
+		return $this->idTarjeta;
+	}
+	public function getSaldo(){
+		return $this->saldo;
+	}
+	public function getTipo(){
+		return $this->tipos[$this->tipo];
+	}
+}
+?>
