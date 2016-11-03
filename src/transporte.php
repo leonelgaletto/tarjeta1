ed01b5d900d4fac9754ec8804b42c0d62c326e0c
leonelgaletto <leonel.galetto@gmail.com>
2016-11-03 13:05:18 -0300

    Update transporte.php

--- src/transporte.php
+++ src/transporte.php
@@ -1,8 +1,18 @@
 <?php
 namespace Poli\Tarjeta;
-class Transporte {
-  protected $tipo;
-  public function Tipo(){
-    return $this->tipo;
-  }
+abstract class Transporte {
+	protected $id,$costo,$tipo,$costotrans;
+	public function getId(){
+		return $this->id;
+	}
+	public function getTipo(){
+		return $this->tipo;
+	}
+	public function getCosto(){
+		return $this->costo;
+	}
+	public function getCostoTrans(){
+		return $this->costotrans;
+	}
 }
+?>
